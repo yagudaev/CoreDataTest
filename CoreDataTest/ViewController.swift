762a2dfdd9cc239f0f95711cfa10054d3d818ef3
Michael Yagudaev <michael@yagudaev.com>
2014-08-24 14:17:01 -0700

    Initial Commit

---
+++ CoreDataTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by Michael Yagudaev on 2014-08-24.
+//  Copyright (c) 2014 Michael Yagudaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
